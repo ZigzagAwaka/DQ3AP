e6b004c8f775aea1f064b930669a57b472d265ba
ZigzagAwaka <joledoux18@gmail.com>
2026-08-30 12:57:00 +0200

    Improved responsiveness of client

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -118,7 +118,7 @@ static DWORD WINAPI MainThread(LPVOID)
                 apClient.Update();
             }
 
-            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+            std::this_thread::sleep_for(std::chrono::milliseconds(250));
         }
 
         // Cleanup and close files/thread
