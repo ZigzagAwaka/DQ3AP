03225112024d2177a07549fd7a287c8e70c9b65d
ZigzagAwaka <joledoux18@gmail.com>
2026-09-12 18:53:18 +0200

    Added randomization effect of shiny spots

--- LuaScript/Nicola_EventCommonScript.lua
+++ LuaScript/Nicola_EventCommonScript.lua
@@ -651,22 +651,69 @@ end
 function SearchObject_Shine_Sea(BeginOverlap, table, CanObtainItem, TreasureId, ItemId1, ItemCount1, Gold1, ItemId2, ItemCount2, Gold2, ItemId3, ItemCount3, Gold3)
   local eventInfo = EventStart(table, "")
   print(tostring(CanObtainItem) .. ", " .. TreasureId .. ", " .. ItemId1 .. ", " .. tostring(ItemCount1) .. ", " .. tostring(Gold1) .. ", " .. ItemId2 .. ", " .. tostring(ItemCount2) .. ", " .. tostring(Gold2) .. ", " .. ItemId3 .. ", " .. tostring(ItemCount3) .. ", " .. tostring(Gold3))
-  SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId1, ItemCount1, Gold1)
-  SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId2, ItemCount2, Gold2)
-  SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId3, ItemCount3, Gold3)
+  -- AP
+  local checkName = ""
+  local isExcluded = AP.IsLocationExcluded(TreasureId)
+  AP.Log("SearchObject_Shine_Sea called with TreasureId: " .. tostring(TreasureId))
+  if 0 < ItemCount1 or 0 < Gold1 then
+    SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId1, ItemCount1, Gold1, isExcluded)
+    checkName = checkName .. TreasureId .. "_1"
+  end
+  if 0 < ItemCount2 or 0 < Gold2 then
+    SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId2, ItemCount2, Gold2, isExcluded)
+    checkName = checkName .. "\n" .. TreasureId .. "_2"
+  end
+  if 0 < ItemCount3 or 0 < Gold3 then
+    SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId3, ItemCount3, Gold3, isExcluded)
+    checkName = checkName .. "\n" .. TreasureId .. "_3"
+  end
+  if not isExcluded then
+    AP.CheckLocation(checkName)
+    AP.GiveItemsIfAvailable(GetTargetActorIdFromEventInfo(eventInfo), TreasureId)
+  end
+  -- AP end
   EventEnd(eventInfo, "")
 end
 
 function SearchObject_Shine_Ground(BeginOverlap, table, CanObtainItem, TreasureId, ItemId1, ItemCount1, Gold1, ItemId2, ItemCount2, Gold2, ItemId3, ItemCount3, Gold3)
   local eventInfo = EventStart(table, "")
   print(tostring(CanObtainItem) .. ", " .. TreasureId .. ", " .. ItemId1 .. ", " .. tostring(ItemCount1) .. ", " .. tostring(Gold1) .. ", " .. ItemId2 .. ", " .. tostring(ItemCount2) .. ", " .. tostring(Gold2) .. ", " .. ItemId3 .. ", " .. tostring(ItemCount3) .. ", " .. tostring(Gold3))
-  SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId1, ItemCount1, Gold1)
-  SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId2, ItemCount2, Gold2)
-  SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId3, ItemCount3, Gold3)
+  -- AP
+  local checkName = ""
+  local isExcluded = AP.IsLocationExcluded(TreasureId)
+  AP.Log("SearchObject_Shine_Ground called with TreasureId: " .. tostring(TreasureId))
+  if 0 < ItemCount1 or 0 < Gold1 then
+    SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId1, ItemCount1, Gold1, isExcluded)
+    checkName = checkName .. TreasureId .. "_1"
+  end
+  if 0 < ItemCount2 or 0 < Gold2 then
+    SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId2, ItemCount2, Gold2, isExcluded)
+    checkName = checkName .. "\n" .. TreasureId .. "_2"
+  end
+  if 0 < ItemCount3 or 0 < Gold3 then
+    SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId3, ItemCount3, Gold3, isExcluded)
+    checkName = checkName .. "\n" .. TreasureId .. "_3"
+  end
+  if not isExcluded then
+    AP.CheckLocation(checkName)
+    AP.GiveItemsIfAvailable(GetTargetActorIdFromEventInfo(eventInfo), TreasureId)
+  end
+  -- AP end
   EventEnd(eventInfo, "")
 end
 
-function SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId, ItemCount, Gold)
+function SearchObject_Shine_ShowMessage(eventInfo, TreasureId, ItemId, ItemCount, Gold, IsExcluded)
+  -- AP
+  if not IsExcluded then
+    ItemId = "ITEM_ARCHIPELAGO"
+    SetTagItemId(ItemId)
+    CmdLoadItemIcon(ItemId)
+    CmdPlayItemGetNoWait(GetTargetActorIdFromEventInfo(eventInfo), TreasureId)
+    PlaySEUI("SYSSE_TD_TREASURE_BOX_ITEM")
+    CmdEventClosingMessage("NPC_Talk_Common_SEARCHOBJECT_TREASURE_11")
+    return
+  end
+  -- AP end
   if 0 < ItemCount then
     local result = AddItemDetail(ItemId, ItemCount, -1, true)
     SetTagItemId(ItemId)
